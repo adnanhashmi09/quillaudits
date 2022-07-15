da2750e5924e9ab5a887a6893f9ef04127ae9620
Adnan Hashmi <hashmi.adnan791@gmail.com>
2022-07-16 01:10:17 +0530

    added favicon

--- components/utils/Meta.tsx
+++ components/utils/Meta.tsx
@@ -1,12 +1,18 @@
 // react/next imports
 import Head from 'next/head';
 
-const Meta = ({ title, description }: { title: string; description: string }) => {
+const Meta = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
   return (
     <Head>
       <title>Project | {title} </title>
       <meta name="description" content={description} />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
     </Head>
   );
 };
